f14ac3291e5a5a9df2c3a05270441665616abe3d
jtmPC <jon87tyler@gmail.com>
2017-12-28 17:23:59 -0700

    playing w adding one row at a time

--- bundle.js
+++ bundle.js
@@ -73,7 +73,15 @@
 
 	      expensesTable.getHeaderRowRange().values = [["Date", "Merchant", "Category", "Amount"]];
 
-	      expensesTable.rows.add(null /*add at the end*/, [["1/1/2017", "The Phone Company", "Communications", "120"], ["1/2/2017", "Northwind Electric Cars", "Transportation", "142.33"], ["1/5/2017", "Best For You Organics Company", "Groceries", "27.9"], ["1/10/2017", "Coho Vineyard", "Restaurant", "33"], ["1/11/2017", "Bellows College", "Education", "350.1"], ["1/15/2017", "Trey Research", "Other", "135"], ["1/15/2017", "Best For You Organics Company", "Groceries", "97.88"]]);
+	      var valuesToAdd1 = [["1/1/2017", "The Phone Company", "Communications", "120"]];
+
+	      var valuesToAdd2 = [["1/2/2017", "Northwind Electric Cars", "Transportation", "142.33"]];
+
+	      expensesTable.rows.add(null, valuesToAdd1);
+	      expensesTable.rows.add(null, valuesToAdd2);
+
+	      console.log("Expenses Table -> Rows", expensesTable.rows);
+	      console.log("range to be formatted", expensesTable.columns.getItemAt(3).getRange());
 
 	      expensesTable.columns.getItemAt(3).getRange().numberFormat = [['€#,##0.00']];
 	      expensesTable.getRange().format.autofitColumns();
